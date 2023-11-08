dc1d3f617ac9cd2120b7fd763f1d40beb37417f1
DataGeorg <georgradic@yahoo.de>
2023-11-08 12:02:15 +0100

    Kommantare der Absoluten Pfade für Team-Member PCs

--- grundgerüst.py
+++ grundgerüst.py
@@ -4,6 +4,10 @@ def load_menu(menu_file):
     food_menu = pd.read_csv(menu_file, index_col=0)
     return food_menu
 
-menu_file = r"C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv"
+# "C:\Users\User\Documents\GitHub\gastro\speisekarte.csv" ## Georg's PC
+# "C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv ## Florian's PC
+# "C:\Users\User\Documents\Thorsten\GitHub\gastro\speisekarte.csv ## Thorsten's PC
+menu_file = r"C:\Users\User\Documents\GitHub\gastro\speisekarte.csv"
+
 food_menu = load_menu(menu_file)
 print(food_menu)
