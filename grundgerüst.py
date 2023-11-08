2730b69f48ea2766ad65da5ab9b793f2a507fc02
Flo <f.kurek2403@outlook.de>
2023-11-08 11:27:09 +0100

    Refactoring Speisekarte

--- grundgerüst.py
+++ grundgerüst.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def load_menu(menu_file, encoding="utf-8"):
-    speisekarte = pd.read_csv(menu_file, encoding=encoding)
-    return speisekarte
+    menu = pd.read_csv(menu_file, encoding=encoding)
+    return menu
 
 menu_file = r"speisekarte.csv"
-speisekarte = load_menu(menu_file)
+restaurant_menu = load_menu(menu_file)
 
 load_menu(menu_file)
-print(speisekarte)
+print(restaurant_menu)
