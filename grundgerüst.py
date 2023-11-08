db2f4d6b7f48dadb191fc4fd281510f1fad9d5a7
Flo <f.kurek2403@outlook.de>
2023-11-08 14:17:10 +0100

    Bezahlfunktion #7

--- grundgerüst.py
+++ grundgerüst.py
@@ -43,3 +43,32 @@ def cancel_order(order_list, order_id):
     return order_list
 # endregion Storno
 
+# region Bezahlen und Rechnung erstellen
+def calculate_total(order, menu):
+    total = 0
+    for i, row in order.iterrows():
+        item_id = row["SpeiseID"]
+        quantity = row["Menge"]
+        price = menu.loc[item_id, "Preis"]
+        total += price * quantity
+    return total
+
+def generate_invoice(order, menu, tip=0):
+    total = calculate_total(order, menu)
+    net_price = total
+    tax_rate = 0.19  # Beispiel: 19% Mehrwertsteuer
+    tax_amount = total * tax_rate
+    total_price = total + tax_amount
+    if tip > 0:
+        total_price += tip
+
+    invoice = {
+        "Gerichte": order,
+        "Nettopreis": net_price,
+        "MwSt.": tax_amount,
+        "Bruttopreis": total_price,
+        "Trinkgeld": tip
+    }
+    return invoice
+# endregion Bezahlen und Rechnung erstellen
+
