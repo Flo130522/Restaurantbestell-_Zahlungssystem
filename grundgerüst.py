32646f0e2763f690e5dad7adc456919c1b3bc1bb
Flo <f.kurek2403@outlook.de>
2023-11-08 11:09:32 +0100

    CSV angepasst

--- grundgerüst.py
+++ grundgerüst.py
@@ -4,6 +4,6 @@ def load_menu(menu_file):
     speisekarte = pd.read_csv(menu_file, index_col=0)
     return speisekarte
 
-menu_file = r"C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv"  # Verwenden Sie den absoluten Dateipfad
+menu_file = r"C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv"
 speisekarte = load_menu(menu_file)
 print(speisekarte)
