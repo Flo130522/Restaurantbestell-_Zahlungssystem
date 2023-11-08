5a90c4dfaf3582ff56f82d3a599d6b31dab8ec93
Flo <f.kurek2403@outlook.de>
2023-11-08 15:31:33 +0100

    Bezahlfunktion #7

--- grundgerüst.py
+++ grundgerüst.py
@@ -4,6 +4,7 @@ import pandas as pd
 # endregion Modulimporte
 
 # region Speisekarte laden
+restaurant_name = "Golden Seagull"
 def load_menu(menu_file, encoding="utf-8", index_col="id"):
     menu = pd.read_csv(menu_file, encoding=encoding)
     return menu
@@ -14,13 +15,12 @@ print(restaurant_menu)
 #endregion Speisekarte laden
 
 # region Bestellungen erstellen und Bestellliste anzeigen
-bestellungen = pd.DataFrame(columns=["ID", "Datum", "Tischnummer", "SpeiseID", "Menge", "Status"])
+bestellungen = pd.DataFrame(columns=["ID", "Datum", "SpeiseID", "Menge", "Status"])
 
-def create_order(speiseID, menge, tischnummer):
+def create_order(speiseID, menge):
     global bestellungen
     bestellungen = bestellungen.append({"ID": len(bestellungen) + 1,
                                         "Datum": datetime.now(),
-                                        "Tischnummer": tischnummer,
                                         "SpeiseID": speiseID,
                                         "Menge": menge,
                                         "Status": "offen"}, ignore_index=True)
@@ -53,7 +53,7 @@ def calculate_total(order, menu):
         total += price * quantity
     return total
 
-def generate_invoice(order, menu, tip=0):
+def generate_invoice(order, menu, tip=0, restaurant_name=""):
     total = calculate_total(order, menu)
     net_price = total
     tax_rate = 0.19  # Beispiel: 19% Mehrwertsteuer
@@ -62,13 +62,39 @@ def generate_invoice(order, menu, tip=0):
     if tip > 0:
         total_price += tip
 
-    invoice = {
-        "Gerichte": order,
-        "Nettopreis": net_price,
-        "MwSt.": tax_amount,
-        "Bruttopreis": total_price,
-        "Trinkgeld": tip
-    }
+    invoice = f"Restaurant: {restaurant_name}\n"
+    invoice += "-----------------------------\n"
+    invoice += order.to_string(index=False) + "\n"
+    invoice += "-----------------------------\n"
+    invoice += f"Gesamtpreis: {total:.2f} €\n"
+    invoice += f"Trinkgeld: {tip:.2f} €\n"
+    invoice += f"Nettopreis: {net_price:.2f} €\n"
+    invoice += f"MwSt.: {tax_amount:.2f} €\n"
+    invoice += f"Bruttopreis: {total_price:.2f} €\n"
+
     return invoice
+
 # endregion Bezahlen und Rechnung erstellen
 
+if __name__ == "__main__":
+    menu_file = "speisekarte.csv"
+    menu = load_menu(menu_file)
+
+    order_list = pd.DataFrame(columns=["ID", "Datum", "Tischnummer", "SpeiseID", "Menge", "Status"])
+
+    order_details = {
+        1: 2,  # Beispiel: SpeiseID und Menge
+        3: 1
+    }
+
+    if validate_order(menu, order_details):
+        order_list = create_order(order_list, table_number=1, menu=menu, order_details=order_details)
+    else:
+        print("Ungültige Bestellung.")
+
+    order_id_to_cancel = 1  # Beispiel: Bestellung stornieren
+    cancel_order(order_list, order_id_to_cancel)
+
+    tip_amount = 5  # Beispiel: Trinkgeld
+    invoice = generate_invoice(order_list, menu, tip_amount)
+    print(invoice)
