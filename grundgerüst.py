2d451b0a8bd0780728ff45b375d20fdddf5ca095
DataGeorg <georgradic@yahoo.de>
2023-11-08 11:30:07 +0100

    german var into english var translated

--- grundgerüst.py
+++ grundgerüst.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def load_menu(menu_file):
-    speisekarte = pd.read_csv(menu_file, index_col=0)
-    return speisekarte
+    food_menu = pd.read_csv(menu_file, index_col=0)
+    return food_menu
 
 menu_file = r"C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv"
-speisekarte = load_menu(menu_file)
-print(speisekarte)
+food_menu = load_menu(menu_file)
+print(food_menu)
