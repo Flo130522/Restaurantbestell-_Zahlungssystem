da419f164b6e7c2e89b885c900f22f95660000a0
Flo <f.kurek2403@outlook.de>
2023-11-08 11:20:42 +0100

    #

--- grundgerüst.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_menu(menu_file):
-    speisekarte = pd.read_csv(menu_file, index_col=0)
-    return speisekarte
-
-menu_file = r"C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv"
-speisekarte = load_menu(menu_file)
-print(speisekarte)
