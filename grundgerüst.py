1d60cbd39131497e6e14a10e400a8ebc6934805d
Flo <f.kurek2403@outlook.de>
2023-11-08 11:25:50 +0100

    Speisekarte wird angezeigt

--- grundgerüst.py
+++ grundgerüst.py
@@ -4,5 +4,8 @@ def load_menu(menu_file, encoding="utf-8"):
     speisekarte = pd.read_csv(menu_file, encoding=encoding)
     return speisekarte
 
-menu_file = r"C:\Users\Admin\source\repos\Flo130522\gastro\speisekarte.csv"
+menu_file = r"speisekarte.csv"
 speisekarte = load_menu(menu_file)
+
+load_menu(menu_file)
+print(speisekarte)
