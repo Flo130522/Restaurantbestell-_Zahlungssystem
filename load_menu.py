aac44d1178d0537ccaf0e7acff82c5e9dae96546
Flo <f.kurek2403@outlook.de>
2023-11-08 12:11:49 +0100

    #6 getrennt

---
+++ load_menu.py
@@ -0,0 +1,7 @@
+def load_menu(menu_file, encoding="utf-8", index_col="id"):
+    menu = pd.read_csv(menu_file, encoding=encoding)
+    return menu
+
+menu_file = r"speisekarte.csv"
+restaurant_menu = load_menu(menu_file)
+print(restaurant_menu)
