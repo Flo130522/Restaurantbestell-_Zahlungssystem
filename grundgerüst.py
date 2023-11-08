e5969f3e1b415d82ea2de10a40ea6f4def8ec81b
tschmidt66 <149807025+tschmidt66@users.noreply.github.com>
2023-11-08 13:12:18 +0100

    Fehler behoben

--- grundgerüst.py
+++ grundgerüst.py
@@ -1,5 +1,8 @@
 import pandas as pd
-
+import os
+print(os.getcwd())
+os.chdir(r"c:/Users/User/Documents/Thorsten/GitHub/gastro")
+print(os.getcwd())
 def load_menu(menu_file, encoding="utf-8"):
     menu = pd.read_csv(menu_file, encoding=encoding)
     return menu
@@ -7,5 +10,5 @@ def load_menu(menu_file, encoding="utf-8"):
 menu_file = r"speisekarte.csv"
 restaurant_menu = load_menu(menu_file)
 
-load_menu(menu_file)
 print(restaurant_menu)
+
