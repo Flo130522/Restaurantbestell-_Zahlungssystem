92c55d08c36691eec1c140bee4f49193398f6706
tschmidt66 <149807025+tschmidt66@users.noreply.github.com>
2023-11-08 14:12:17 +0100

    erste Datenvalidierung

--- grundgerüst.py
+++ grundgerüst.py
@@ -12,3 +12,31 @@ restaurant_menu = load_menu(menu_file)
 
 print(restaurant_menu)
 
+def bestellung_pruefen(speisekarte, bestellung):
+    for gericht in bestellung:
+        if gericht not in speisekarte:
+            return False  # Falls ein Gericht in der Bestellung nicht auf der Speisekarte ist, ist die Bestellung ungültig
+    return True  # Alle Gerichte in der Bestellung sind auf der Speisekarte
+
+# Beispiel für die Speisekarte und eine Bestellung
+speisekarte = ['Schnitzel mit Pommes,Frisches Schweineschnitzel mit knusprigen Pommes frites', 
+               'Spaghetti Bolognese,Italienische Spaghetti mit herzhafter Bolognesesauce', 
+               'Caesar Salat,Gemischter Salat mit Caesar-Dressing und Croutons', 
+               'Pizza Margherita,Klassische Pizza mit Tomaten & Mozzarella und Basilikum', 
+               'Fischfilet mit Gemüse,Gebratenes Fischfilet mit saisonalem Gemüse',
+               "Fruchtsalat,Bunter Fruchtsalat mit frischen Früchten"]
+
+bestellung_1 = ['Pizza', 'Salat', 'Burger']
+bestellung_2 = ['Pasta', 'Suppe', 'Dessert']
+
+# Überprüfung der Bestellungen
+if bestellung_pruefen(speisekarte, bestellung_1):
+    print("Bestellung 1 ist gültig.")
+else:
+    print("Bestellung 1 enthält Gerichte, die nicht auf der Speisekarte sind.")
+
+if bestellung_pruefen(speisekarte, bestellung_2):
+    print("Bestellung 2 ist gültig.")
+else:
+    print("Bestellung 2 enthält Gerichte, die nicht auf der Speisekarte sind.")
+
