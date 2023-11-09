5c9bea971693e51524a3e660c76bde028038d9ce
tschmidt66 <149807025+tschmidt66@users.noreply.github.com>
2023-11-09 09:12:41 +0100

    Änderung

--- grundgerüst.py
+++ grundgerüst.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import os
-print(os.getcwd())
-os.chdir(r"c:/Users/User/Documents/Thorsten/GitHub/gastro")
-print(os.getcwd())
+
 def load_menu(menu_file, encoding="utf-8"):
     menu = pd.read_csv(menu_file, encoding=encoding)
     return menu
