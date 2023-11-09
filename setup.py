4ca91c555e203e03d2b7aec916e0da24f9d6de01
Flo <f.kurek2403@outlook.de>
2023-11-09 08:45:50 +0100

    Tkinter-GUI #16

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-from cx_Freeze import setup, Executable
+from distutils.core import setup
+import py2exe
 
-executables = [Executable("grundgerüst.py", base=None)]
-
-setup(
-    name="Golden Seagull OrderSystem", 
-    version="1.0",
-    description="Beschreibung Ihrer Anwendung",
-    executables=executables,
-    options={
-        "build_exe": {
-            "includes": ["tkinter", "pandas", "datetime"],  
-            "include_files": ["speisekarte.csv"], 
-        }
-    }
-)
+setup(console=['tkinter_test.py'])
