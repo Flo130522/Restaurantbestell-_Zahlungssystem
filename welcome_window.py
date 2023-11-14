159fff748e03179434329309715ad9281e5d7946
Flo <144101306+Flo130522@users.noreply.github.com>
2023-11-14 14:00:37 +0100

    Update welcome_window.py

--- welcome_window.py
+++ welcome_window.py
@@ -5,16 +5,14 @@ class WelcomeWindow:
         self.root = root
         self.root.title("Willkommen bei Golden Seagull")
 
-        # Willkommensnachricht mit Schriftgröße 20
         label = tk.Label(root, text="Willkommen bei Golden Seagull", font=("Arial", 20))
         label.grid()
 
-        # Button mit Schriftgröße 15 und Aktion zum Wechseln zum Hauptmenü
         button = tk.Button(root, text="Jetzt bestellen!", font=("Arial", 15), command=self.open_main_menu)
         button.grid()
 
     def open_main_menu(self):
-        from main_menu import MainMenu
+        from grundgerüst import MainMenu
         main_menu = MainMenu(self.root)
 
 if __name__ == "__main__":
